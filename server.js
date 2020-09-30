b1f6a0a9ee57dd74cb15494bad1c8f00ea339348
Opilane TTHK <opilane@tthk.ee>
2020-09-30 11:24:54 +0300

    m

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const app = express();
 const axios = require('axios');
-const bParser = require('bodyparser');
+const bParser = require('body-parser');
 
 app.use(express.static("public"));
 app.set('view engine',ejs);
@@ -15,22 +15,22 @@ app.post('/country', (req, res) =>{
     let link = "https://restcountries.eu/rest/v2/name/estonia?fullText=true";
     axios.get(link)
     .then(function(response){
-        let name = response.name;
-        let topLevelDomain = response.topLevelDomain.alpha2Code;
-        let callingCode = response.callingCodes[0];
-        let capital = response.capital;
-        let region = response.region;
-        let subregion = response.subregion;
-        let population = response.population;
-        let timezone = response.timezones;
-        let languageInaFullWordInEnglish = response.languages[0].name;
+        console.log(response);
+        let name = response.data.name;
+        let topLevelDomain = response.data.alpha2Code;
+        let callingCode = response.data.callingCodes[0];
+        let capital = response.data.capital;
+        let region = response.data.region;
+        let subregion = response.data.subregion;
+        let population = response.data.population;
+        let timezone = response.data.timezones;
+        let languageInaFullWordInEnglish = response.data.languages[0].name;
         let currencyCodeNameAndSymbol = "";
-        let aUrlToTheFlagImage = response.flag;
+        let aUrlToTheFlagImage = response.data.flag;
 
         let country = req.body.country;
-        console.log(response);
-
         
+
     })
     .catch(function(error){
         console.log(error)
